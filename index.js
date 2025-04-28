df9208e60e47d3eafdb8646a7dde1878ff17b717
Charles POULAIN <charles.probe@gmail.com>
2025-04-28 10:17:10 +0200

    Fix: Metrics transition to commonjs

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const fastify = require('fastify')({ logger: true });
+const fastify = require('fastify')();
+const metrics = require('fastify-metrics');
 
 fastify.register(require('./routes/user'));
+fastify.register(metrics, { endpoint: '/metrics' });
 
 fastify.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
 	if (err) {
 		fastify.log.error(err);
 		process.exit(1);
 	}
-	fastify.log.info(`Server listening at ${address}`);
+	fastify.log.info(`fastify listening at ${addr}`);
 });
